32b6f58a2f39cc56b12c820a28ed019434351342
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-03-29 15:35:20 +0300

    Drop Spotless plug-in (#5)
    Will replace it with Diktat and/or Detekt later on.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     id("org.jetbrains.dokka") version "1.6.0"
-    id("com.diffplug.spotless") version "6.0.0"
 
     kotlin("jvm") version "1.6.0" apply false
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.dokka")
-    apply(plugin = "com.diffplug.spotless")
 
     tasks.withType<KotlinCompile> {
-        dependsOn("spotlessApply")
     }
 
     tasks.dokkaHtml.configure {
         // intentionally hide inherited members, otherwise we will have a lot of functions inherited from JNA
         suppressInheritedMembers.set(true)
-    }
-
-    spotless {
-        kotlin {
-            ktfmt().kotlinlangStyle()
-        }
     }
 }
 
