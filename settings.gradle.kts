9419107e4641d66c53a854a45861742e0d1c72a7
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-03-30 15:02:11 +0300

    Enable the gradle-enterprise plug-in

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,4 +10,25 @@
 
 rootProject.name = "kotlin-tree"
 
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.12.5"
+}
+
 include("jna-lib")
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
